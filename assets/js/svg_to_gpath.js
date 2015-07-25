fb5556a351adca42db64cdddd9c30ef6b00ac085
Scott Brackett <scottybrackett@gmail.com>
2015-07-25 16:35:46 -0500

    update svg example code
    changed the example svg from one that has curved lines, to one that doesn't so that the image on the page will reflect what you will see when the polygon is rendered on pebble

--- assets/js/svg_to_gpath.js
+++ assets/js/svg_to_gpath.js
@@ -3,17 +3,18 @@
 
 $(document).ready(function(){
 
-$("#input-code").html(`<svg width="144" height="168" xmlns="http://www.w3.org/2000/svg">
+$("#input-code").html(`<svg width="143.83116883116884" height="167.85714285714286" xmlns="http://www.w3.org/2000/svg">
+ <!--Created with Vector Code http://www.vectorcodeapp.com-->
  <g>
-  <title>wrench</title>
-  <rect fill="none" id="canvas_background" height="170" width="146" y="-1" x="-1"/>
+  <title>background</title>
+  <rect fill="none" id="canvas_background" height="169.85714" width="145.83117" y="-1" x="-1"/>
   <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
    <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
   </g>
  </g>
  <g>
   <title>Layer 1</title>
-  <path id="svg_1" d="m113.14357,77.89882c6.30427,-7.24078 7.57114,-17.12182 4.14189,-25.43174l-12.65578,14.74366l-12.4756,-2.42308l-4.13335,-12.00538l12.6235,-14.70961c-8.68346,-2.18244 -18.24962,0.45235 -24.52604,7.66453c-6.61898,7.60315 -7.70222,18.13226 -3.60543,26.67488l-32.98861,37.89742c-3.51,4.02928 -3.08239,10.14036 0.95093,13.64584c4.03329,3.50272 10.1492,3.07935 13.65794,-0.94995l32.94992,-37.86025c9.07004,2.92064 19.41106,0.39165 26.06064,-7.2463l0,0z" stroke-width="1.5" stroke="#000" fill="#fff"/>
+  <path id="svg_1" d="m28.85999,41.83521l0,-1.29871l11.34373,-14.164l5.15631,0l0,0l24.75003,0l46.40621,0l11.34372,14.164l0,35.41l0,0l0,21.24593l0,0l-11.34372,14.164l-46.40621,0l-32.33283,36.01608l7.5828,-36.01608l-5.15631,0l-11.34373,-14.164l0,0l0,-21.24593l0,0l0,-34.11129z" stroke-width="1.5" stroke="#000" fill="#fff"/>
  </g>
 </svg>`)
 //var $output_coordinates = $("#output-coordinates");
